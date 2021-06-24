5aaf3623c7ca5092c6893e0a556216ffc29d444f
Andres Robayo <morewebdesignrocks@gmail.com>
2021-06-24 16:27:25 -0400

    Change main functionality
    Instead of having objects with all the attributes included in the desired animation, the function will be animation itself and only a few attributes can be changed.

--- GWD-Library.js
+++ GWD-Library.js
@@ -1,52 +1,25 @@
-// Set elements to animate
-const element01 = {elementId: "main-text", xInitialPos: "-300px", xFinalPos: "0", yInitialPos: "0", yFinalPos: "0", transitionTime: "1", delay: "0", stagger: "0.1"};
-const element02 = {elementId: "sub-text", xInitialPos: "-300px", xFinalPos: "0", yInitialPos: "-300px", yFinalPos: "0", transitionTime: "0.8", delay: "0.5", stagger: "0"};
-
-// Convert string of text into individual letter spans
-function textWrapper(s) {
-  s.innerHTML = s.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-}
-
 function splitLetters(e) {
-  // Get elements
-  let element = document.getElementById(e.elementId);
-  
-  // Convert string of text into individual letter spans
-  element.innerHTML = element.textContent.replace(/\S/g, "<span class='letter' style='display: inline-block;'>$&</span>");
-}
-
-// Animation Library
-function animateLetters(e) {
-  //let element = document.getElementById(e.elementId);
-  splitLetters(e);
-  
-  // Get letters spans
-  let elementLetters = document.getElementsByClassName("letter");
-  
-  // Animate individual letter spans
-  gsap.set(".letter", {
-    x: e.xInitialPos, y: e.yInitialPos
-  });
-  gsap.to(".letter", Number(e.transitionTime), {
-    x: e.xFinalPos, y: e.yFinalPos, delay: e.delay, stagger: e.stagger
-  });
-}
-
-
-function dropLetters(e) {
-  splitLetters(e);
-  
-  // Get letters spans
-  let elementLetters = document.getElementsByClassName("letter");
-  
-  // Animate individual letter spans
-  gsap.set(".letter", {
-    y: -100%
-  });
-  gsap.to(".letter", Number(e.transitionTime), {
-    x: e.xFinalPos, y: e.yFinalPos, delay: e.delay, stagger: e.stagger
-  });
-}
+    // Get elements
+    let element = document.getElementById(e);
     
-// Request animations here
-animateLetters(element01);
+    // Convert string of text into individual letter spans
+    element.innerHTML = element.textContent.replace(/\S/g, "<span class='letter' style='display: inline-block;'>$&</span>");
+  }
+
+  // DROP LETTERS
+  function dropLetters(id, duration, delay, stag) { 
+    splitLetters(id);
+    
+    // Get letters spans
+    let targetElement = "#" + id + " .letter";
+    let elementLetters = document.querySelectorAll(targetElement);
+    
+    // Run animation
+    gsap.set(elementLetters, {y:"-1000%"});
+    gsap.to(elementLetters, duration, {y:"0%", delay, stagger: stag});
+  
+  }
+  
+  // Request animation here, include element Id, duration, delay and stagger time) 
+  dropLetters("sub-text", .5, 0.1, 0.2);
+  dropLetters("main-text", .8, 0, 0.3);
