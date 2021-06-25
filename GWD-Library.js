61e0c34f0bd68deb3a771b1f270dd59c4f064ddf
Andres Robayo <morewebdesignrocks@gmail.com>
2021-06-25 10:13:39 -0400

    Add left to right animation and easing

--- GWD-Library.js
+++ GWD-Library.js
@@ -1,25 +1,39 @@
-function splitLetters(e) {
-    // Get elements
-    let element = document.getElementById(e);
-    
-    // Convert string of text into individual letter spans
-    element.innerHTML = element.textContent.replace(/\S/g, "<span class='letter' style='display: inline-block;'>$&</span>");
-  }
+// Get elements
+let element = document.getElementById(e);
+        
+// Convert string of text into individual letter spans
+element.innerHTML = element.textContent.replace(/\S/g, "<span class='letter' style='display: inline-block;'>$&</span>");
+}
 
-  // DROP LETTERS
-  function dropLetters(id, duration, delay, stag) { 
-    splitLetters(id);
-    
-    // Get letters spans
-    let targetElement = "#" + id + " .letter";
-    let elementLetters = document.querySelectorAll(targetElement);
-    
-    // Run animation
-    gsap.set(elementLetters, {y:"-1000%"});
-    gsap.to(elementLetters, duration, {y:"0%", delay, stagger: stag});
-  
-  }
-  
-  // Request animation here, include element Id, duration, delay and stagger time) 
-  dropLetters("sub-text", .5, 0.1, 0.2);
-  dropLetters("main-text", .8, 0, 0.3);
+// DROP LETTERS
+function dropLetters(id, duration, delay, stag) { 
+splitLetters(id);
+
+// Get letters spans
+let targetElement = "#" + id + " .letter";
+let elementLetters = document.querySelectorAll(targetElement);
+
+// Run animation
+gsap.set(elementLetters, { y:"-1000%" });
+gsap.to(elementLetters, duration, { y:"0px", delay, stagger: stag, ease: Bounce.easeOut });
+
+}
+
+// LEFT TO RIGTH LETTERS
+function leftToRightLetters(id, duration, delay, stag) { 
+splitLetters(id);
+
+// Get letters spans
+let targetElement = "#" + id + " .letter";
+let elementLetters = document.querySelectorAll(targetElement);
+
+// Run animation
+gsap.set(elementLetters, { x:"-1000px" });
+gsap.to(elementLetters, duration, { x:"0px", delay, stagger: stag, ease: Elastic.easeOut.config(1.5, 0.5) });
+
+}
+
+// Request animation here, include element Id, duration, delay and stagger time) 
+// Example: leftToRight("id", duration, delay, stagger);
+dropLetters("main-text", .8, 0, 0.3);
+leftToRightLetters("sub-text", .5, 0.1, 0.2);
