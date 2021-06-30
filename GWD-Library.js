0d65b2b3546344d2c1e1ef565af7f44c6c4b4131
Andres Robayo <morewebdesignrocks@gmail.com>
2021-06-30 16:34:59 -0400

    Add Appear and Wave animations

--- GWD-Library.js
+++ GWD-Library.js
@@ -1,23 +1,19 @@
- // Split words into individual letter spans
- function splitLetters(e) {
+// Split words into individual letter spans
+function splitLetters(e) {
     // Get elements
     let element = document.getElementById(e);
     // Convert string of text into individual letter spans
     element.innerHTML = element.textContent.replace(/\S/g, "<span class='letter' style='display: inline-block;'>$&</span>");
   }     
-        
-  function randomIndex(id, yDir, xDir, duration, delay, stag) {
-    splitLetters(id);
-    // Get letters spans
-    let targetElement = "#" + id + " .letter";
-    let elementLetters = document.querySelectorAll(targetElement);
-    console.log(stag);
+  
+  /*
+  // Generate a randomized list of indexes for total amount array items
+  function randomIndex(length) {    
     // new empty array
-    /*
     let numbers = [];
     let n,
         p,
-        r = elementLetters.length,
+        r = length,
         min = 0, 
         max = r - 1; // how many numbers to extract
 
@@ -31,47 +27,34 @@
       }
       while(p);
     }
-    return numbers;
-    */
-    // Run animation
-    let arr = [0,1];
-    for ( let i = 0; i < arr.length; i++) {
+    return numbers;    
+  }
+  */
+  /*
+  function randomLetter(id, yDir, xDir, duration, delay, stag) {
+    splitLetters(id);
+    // Get letters spans
+    let targetElement = "#" + id + " .letter";
+    let elementLetters = document.querySelectorAll(targetElement);
+    let indexNumbers = randomIndex(elementLetters.length);
+    console.log(indexNumbers);
+    
+    for ( let i = 0; i < indexNumbers.length; i++ ) {
+      console.log(indexNumbers[i]);
+      let index = indexNumbers[i];
       
-      // for each iteration console.log a word
-      // and make a pause after it
-      (function (i, stag) {
-        let interval = stag * 1000;
-        console.log(stag);
-        setTimeout(function () {              
-          gsap.from(elementLetters[i], duration, {
-            x: xDir,
-            y: yDir,
-            delay,
-            ease: Circ.easeOut
-          });             
-        }, 500 * i);
-        
-        
-        
-        
-        
-      })(i);
-      
-      /*
-      gsap.from(elementLetters[i], duration, {
+      // Run animation
+      gsap.from(elementLetters[index], duration, {
         x: xDir,
         y: yDir,
         delay,
         stagger: stag,
         ease: Circ.easeOut
       });
-      */
-      
     }
   }
-  //console.log(randomIndex("main-text", "-100px", "0px", 0.3, 0, 1));
-  
-
+  randomLetter("main-text", "-300px", "0px", 0.3, 0, 5);
+  */
   
   
   // Animations Start Here ===== */
@@ -87,7 +70,7 @@
       y: yDir,
       delay: delayTime,
       stagger: stag,
-      ease: "bounce.easeOut"
+      ease: "Bounce.easeOut"
     });
   }
   // ZOOM
@@ -104,7 +87,7 @@
       delay: delayTime,
       fontSize: 0,
       stagger: stag,
-      ease: "sine.easeOut"
+      ease: "Sine.easeOut"
     });
   }
   // FALL
@@ -132,10 +115,60 @@
       },
       delay: delayTime,
       stagger: stag,
-      ease: "sine.inOut"
+      ease: "Sine.inOut"
+    });
+  }
+  // APPEAR
+  function appear(id, yDir, xDir, duration, delayTime, stag) {
+    splitLetters(id);
+    // Get letters spans
+    let targetElement = "#" + id + " .letter";
+    let elementLetters = document.querySelectorAll(targetElement);
+    // Run animation
+    gsap.fromTo(elementLetters, {
+      opacity: 0,
+      x: xDir,
+      y: yDir,    
+    }, {
+      duration: duration,
+      opacity: 1,
+      x: 0,
+      y: 0,
+      delay: delayTime,
+      stagger: stag,
+      ease: "Back.easeOut.config(1.7)"
+    });
+  }
+  // WAVE
+  function wave(id, yDir, xDir, duration, delayTime, stag) {
+    splitLetters(id);
+    // Get letters spans
+    let targetElement = "#" + id + " .letter";
+    let elementLetters = document.querySelectorAll(targetElement);
+    // Run animation
+    let tl = gsap.timeline({repeat: 0, repeatDelay: 1});
+    tl.from(elementLetters, duration, {
+      x: xDir,
+      y: yDir,
+      delay: delayTime,
+      fontSize: "0.5em",
+      stagger: stag,
+      ease: "Sine.easeOut"
+    });
+    tl.to(elementLetters, duration, {
+      delay: delayTime,
+      fontSize: "0.5em",
+      stagger: stag,
+      ease: "Sine.easeOut"
+    });
+    tl.to(elementLetters, duration, {
+      delay: delayTime,
+      fontSize: "1em",
+      stagger: stag,
+      ease: "Sine.easeOut"
     });
   }
   
   // Request animation here, include element Id, vertical position, horizontal position, duration, delay and stagger time) 
   // Example: bounceLetters("id", "-300", "0", 0.5, 0, 0.3);
-  fall("main-text", "-300px", "0px", 2.5, 0, 0);
+  wave("main-text", "0", "0", 0.01, 0.1, 0.1);
